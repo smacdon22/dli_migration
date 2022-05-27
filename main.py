742f349a324cb5a8124072ca925b4ad92be99ca5
sarah:) <89022489+smacdon22@users.noreply.github.com>
2022-05-27 14:37:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,38 +2,40 @@ import json
 import csv
 import requests
 
-
-#change this to your API token
-apitoken = "dc743029-1d2b-4f6b-b938-9b6877564709"
+# change this to your API token
+apitoken = "675907dd-4fa3-414c-a1fc-1f9966ddca6e"
 apiHeader = {"X-Dataverse-key": apitoken}
 requestURL = "https://demodv.scholarsportal.info/api/dataverses/DLI-IDD/datasets"
 # list of persistenIds to publish datasets after uploading
 PIDs = []
+
+false = bool(0)
+true = bool(1)
 
 
 # gets the common info that all imports should have
 # title, production date, description, language, production place, subject (Social Sciences)
 def getTDDLKPS(p):
     # title
-    title_metadata = {"typeName": "title", "multiple": False,
+    title_metadata = {"typeName": "title", "multiple": false,
                       "value": p[1].replace("\xa0", " "), "typeClass": "primitive"}
     # date
-    production_date_metadata = {"typeName": "productionDate", "multiple": False, "value": p[13].replace("\xa0", " "),
+    production_date_metadata = {"typeName": "productionDate", "multiple": false, "value": p[13].replace("\xa0", " "),
                                 "typeClass": "primitive"}
     # description
-    description_value = {"value": p[14].replace("\xa0", " "), "multiple": False, "typeClass": "primitive",
+    description_value = {"value": p[14].replace("\xa0", " "), "multiple": false, "typeClass": "primitive",
                          "typeName": "dsDescriptionValue"}
-    description_metadata = {"typeName": "dsDescription", "multiple": True,
+    description_metadata = {"typeName": "dsDescription", "multiple": true,
                             "value": [{"dsDescriptionValue": description_value}], "typeClass": "compound"}
     # language
-    language_metadata = {"typeName": "language", "multiple": True, "typeClass": "controlledVocabulary",
+    language_metadata = {"typeName": "language", "multiple": true, "typeClass": "controlledVocabulary",
                          "value": [p[25].replace("\xa0", " ")]}
     # place
-    production_place_metadata = {"typeName": "productionPlace", "multiple": False, "typeClass": "primitive",
+    production_place_metadata = {"typeName": "productionPlace", "multiple": false, "typeClass": "primitive",
                                  "value": p[3].replace("\xa0", " ")}
     # subject
-    subject_metadata = {"typeName": "subject", "multiple": True, "value": ["Social Sciences"],
-                        "typeClasubject_metadata": "controlledVocabulary"}
+    subject_metadata = {"typeName": "subject", "multiple": true, "value": ["Social Sciences"],
+                        "typeClass": "controlledVocabulary"}
     return title_metadata, production_date_metadata, description_metadata, \
            language_metadata, production_place_metadata, subject_metadata
 
@@ -51,19 +53,19 @@ def getAuthor(p):
             # if there's another author
             if p[4 + pl] != "":
                 # affiliation
-                author_affiliation = {"typeName": "authorAffiliation", "multiple": False,
+                author_affiliation = {"typeName": "authorAffiliation", "multiple": false,
                                       "value": p[5 + pl].replace("\xa0", " "),
                                       "typeClass": "primitive"}
                 # name
-                author_name = {"typeName": "authorName", "multiple": False, "value": p[4 + pl].replace("\xa0", " "),
+                author_name = {"typeName": "authorName", "multiple": false, "value": p[4 + pl].replace("\xa0", " "),
                                "typeClass": "primitive"}
                 # if there's an ORCID
                 if p[6 + pl] != "":
                     # scheme (ORCID)
-                    author_id_scheme = {"typeName": "authorIdentifierScheme", "multiple": False,
+                    author_id_scheme = {"typeName": "authorIdentifierScheme", "multiple": false,
                                         "typeClass": "controlledVocabulary", "value": "ORCID"}
                     # id
-                    author_id = {"typeName": "authorIdentifier", "multiple": False,
+                    author_id = {"typeName": "authorIdentifier", "multiple": false,
                                  "value": p[6 + pl].replace("\xa0", " "),
                                  "typeClass": "primitive"}
                     authors_metadata.append({"authorAffiliation": author_affiliation, "authorName": author_name,
@@ -73,36 +75,36 @@ def getAuthor(p):
                     authors_metadata.append({"authorAffiliation": author_affiliation, "authorName": author_name})
     else:
         # only author name
-        author_affiliation = {"typeName": "authorAffiliation", "multiple": False, "value": p[5].replace("\xa0", " "),
+        author_affiliation = {"typeName": "authorAffiliation", "multiple": false, "value": p[5].replace("\xa0", " "),
                               "typeClass": "primitive"}
         # only author affiliation
-        author_name = {"typeName": "authorName", "multiple": False, "value": p[4].replace("\xa0", " "),
+        author_name = {"typeName": "authorName", "multiple": false, "value": p[4].replace("\xa0", " "),
                        "typeClass": "primitive"}
         # if there's an ORCID
         if p[6] != "":
-            author_id_scheme = {"typeName": "authorIdentifierScheme", "multiple": False,
+            author_id_scheme = {"typeName": "authorIdentifierScheme", "multiple": false,
                                 "typeClass": "controlledVocabulary",
                                 "value": "ORCID"}
-            author_id = {"typeName": "authorIdentifier", "multiple": False, "value": p[6].replace("\xa0", " "),
+            author_id = {"typeName": "authorIdentifier", "multiple": false, "value": p[6].replace("\xa0", " "),
                          "typeClass": "primitive"}
             authors_metadata = {"authorAffiliation": author_affiliation, "authorName": author_name,
                                 "authorIdentifierScheme": author_id_scheme, "authorIdentifier": author_id}
         else:
             authors_metadata = {"authorAffiliation": author_affiliation, "authorName": author_name}
     # return author metadata
-    author_metadata = {"typeName": "author", "multiple": True, "value": [authors_metadata], "typeClass": "compound"}
+    author_metadata = {"typeName": "author", "multiple": true, "value": [authors_metadata], "typeClass": "compound"}
     return author_metadata
 
 
 # returns contact info
 # could be authors, or DLI
 def getContact(p):
-    contact_email = {"typeClass": "primitive", "multiple": False, "typeName": "datasetContactEmail",
+    contact_email = {"typeClass": "primitive", "multiple": false, "typeName": "datasetContactEmail",
                      "value": "mvail@stfx.ca"}
-    contact_name = {"typeClass": "primitive", "multiple": False, "typeName": "datasetContactName", "value": p[4]}
+    contact_name = {"typeClass": "primitive", "multiple": false, "typeName": "datasetContactName", "value": p[4]}
     # returns dataset contact name and email
     contact_metadata = {"value": [{"datasetContactEmail": contact_email, "datasetContactName": contact_name}],
-                        "typeClass": "compound", "multiple": True,
+                        "typeClass": "compound", "multiple": true,
                         "typeName": "datasetContact"}
     return contact_metadata
 
@@ -117,10 +119,10 @@ def getKeyword(p):
         # if there's another
         if i != "":
             keywords_metadata.append(
-                {"keywordValue": {"typeName": "keywordValue", "multiple": False, "typeClass": "primitive",
+                {"keywordValue": {"typeName": "keywordValue", "multiple": false, "typeClass": "primitive",
                                   "value": i.replace("\xa0", " ")}})
     # returns keywords_metadataeywords
-    keyword_metadata = {"typeName": "keyword", "multiple": True, "typeClass": "compound", "value": keywords_metadata}
+    keyword_metadata = {"typeName": "keyword", "multiple": true, "typeClass": "compound", "value": keywords_metadata}
     return keyword_metadata
 
 
@@ -173,8 +175,9 @@ if __name__ == '__main__':
             prev_ids.append(dataset_id)
             # sets citation fields
             # required: title, description, subject, author, contact
-            blank_json["datasetVersion"]["metadataBlocks"]["citation"]["fields"] = \
-                [title_md, date_md, desc_md, subject_md, author_md, language_md, place_md, contact_md, keyword_md]
+            blank_json["datasetVersion"]["metadataBlocks"]["citation"]["fields"] = [title_md, date_md, desc_md,
+                                                                                    subject_md, author_md, language_md,
+                                                                                    place_md, contact_md, keyword_md]
             # add to list of all metadata
             datasets_metadata.append(blank_json)
     # for each metadata string (in json format)
@@ -182,9 +185,9 @@ if __name__ == '__main__':
         # makes file
         # not needed if straight uploading
         # comment out file code below to, skip that stuff if you want
-        outfile = open("dli_migration_" + prev_ids[c] + ".json", "w")
-        json.dump(i, outfile, indent=4)
-        outfile.close()
+        # outfile = open("dli_migration_" + prev_ids[c] + ".json", "w")
+        # json.dump(i, outfile, indent=4)
+        # outfile.close()
         json.dumps(i)
         apiJSON = i
         # upload dataset as draft
